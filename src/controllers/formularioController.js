fe3e172a2832d8b1dcd0d891f40a794aaa343b1c
Danilo Andrade <danilo@casadosaber.com.br>
2022-06-13 12:23:37 -0300

    Verificando duplicidade

--- src/controllers/formularioController.js
+++ src/controllers/formularioController.js
@@ -2,19 +2,23 @@ import form from "../models/Formulario.js";
 
 class FormController {
 
-    static listarCandidatos = (req,res) => {
+    static listarCandidatos = (req, res) => {
         form.find()
-            .sort({created_at: 1})            
+            .sort({ created_at: 1 })
             .exec((err, form) => {
-            res.status(200).json(form)
-        })    
+                res.status(200).json(form)
+            })
     }
 
-    static cadastrarCandidato = (req,res) => {
+    static cadastrarCandidato = (req, res) => {
         let candidato = new form(req.body);
         candidato.save((err) => {
             if (err) {
-                res.status(500).send({message: `${err.message} - falha ao cadastrar a candidato`})
+                if (err.code === 11000) {
+                    res.status(401).render('pages/duplicado');
+                } else {
+                    res.status(500).send({ message: `${err.number} - falha ao cadastrar a candidato` })
+                }
             } else {
                 res.status(201).redirect("obrigado");
             }
